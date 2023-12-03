48b62f16d571c9db5935b102cf75ae709b9cf0b4
Xavier Fornes <fornes@adobe.com>
2023-12-03 19:36:14 +0100

    fixup! Day02

---
+++ src/main/kotlin/Day02.kt
@@ -0,0 +1,11 @@
+class Day02 {
+    companion object {
+        fun part1(input: List<String>): Int {
+            return 42
+        }
+
+        fun part2(input: List<String>): Int {
+            return 42
+        }
+    }
+}
---
+++ src/test/kotlin/Day02Test.kt
@@ -0,0 +1,12 @@
+import Day02.Companion.part1
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class Day02Test {
+    @Test
+    fun runParts() {
+        val testInputPart1 = readInput("Day02_test_part1")
+        val testResultPart1 = part1(testInputPart1)
+        Assertions.assertEquals(42, testResultPart1)
+    }
+}
