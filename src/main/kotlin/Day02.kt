92f674a10e3f2cbea3c77d0254337df7186f50fb
Xavier Fornes <fornes@adobe.com>
2023-12-03 20:31:30 +0100

    fixup! Day02

--- src/main/kotlin/Day02.kt
+++ src/main/kotlin/Day02.kt
@@ -38,63 +38,63 @@ class Day02 {
                     it
                 }
         }
+
+        private fun <T> Iterable<T>.prodOf(selector: (T) -> Long): Long {
+            var prod: Long = 1.toLong()
+            for (element in this) {
+                prod *= selector(element)
+            }
+            return prod
+        }
+
+        private fun String.minCubeConfiguration(): Map<String, Long> {
+            // ie: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+            val occur = this
+                .split(";")
+                .flatMap { it.split(",") }
+                .map {
+                    // ie: 1 red
+                    val occ = it.trim().split(" ")
+                    Pair(occ.first().toLong(), occ.last())
+                }
+
+            return mapOf(
+                "red" to occur
+                    .filter { "red" == it.second.trim() }
+                    .maxOf {
+                        it.first
+                    },
+                "green" to occur
+                    .filter { "green" == it.second.trim() }
+                    .maxOf {
+                        it.first
+                    },
+                "blue" to occur
+                    .filter { "blue" == it.second.trim() }
+                    .maxOf {
+                        it.first
+                    },
+            )
+        }
+
+        private fun String.areAllGamesPossible(cubes: Map<String, Int>): Boolean {
+            return this.split(";")
+                .all { game ->
+                    game.isGamePossible(cubes)
+                }
+        }
+
+        private fun String.isGamePossible(cubes: Map<String, Int>): Boolean {
+            return this.split(",")
+                .map {
+                    val parts = it.trim().split(" ")
+                    val many = parts.first().trim().toInt()
+                    val color = parts.last().trim()
+                    Pair(many, color)
+                }
+                .all {
+                    it.first <= cubes.getOrDefault(it.second, 0)
+                }
+        }
     }
-}
-
-private fun <T> Iterable<T>.prodOf(selector: (T) -> Long): Long {
-    var prod: Long = 1.toLong()
-    for (element in this) {
-        prod *= selector(element)
-    }
-    return prod
-}
-
-private fun String.minCubeConfiguration(): Map<String, Long> {
-    // ie: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
-    val occur = this
-        .split(";")
-        .flatMap { it.split(",") }
-        .map {
-            // ie: 1 red
-            val occ = it.trim().split(" ")
-            Pair(occ.first().toLong(), occ.last())
-        }
-
-    return mapOf(
-        "red" to occur
-            .filter { "red" == it.second.trim() }
-            .maxOf {
-                it.first
-            },
-        "green" to occur
-            .filter { "green" == it.second.trim() }
-            .maxOf {
-                it.first
-            },
-        "blue" to occur
-            .filter { "blue" == it.second.trim() }
-            .maxOf {
-                it.first
-            },
-    )
-}
-
-private fun String.areAllGamesPossible(cubes: Map<String, Int>): Boolean {
-    return this.split(";")
-        .all { game ->
-            game.isGamePossible(cubes)
-        }
-}
-
-private fun String.isGamePossible(cubes: Map<String, Int>): Boolean {
-    return this.split(",")
-        .map {
-            val parts = it.trim().split(" ")
-            val many = parts.first().trim().toInt()
-            val color = parts.last().trim()
-            Pair(many, color)
-        }
-        .all {
-            it.first <= cubes.getOrDefault(it.second, 0)
-        }
 }
